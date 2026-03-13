e40cf5422dfe7b17a8625eb818f4c29312afa1f3
kokobear <dries.hugaerts@live.be>
2026-03-14 00:38:31 +0100

    flared up the ping command.

--- controller/src/main/java/com/bearify/controller/commands/PingCommand.java
+++ controller/src/main/java/com/bearify/controller/commands/PingCommand.java
@@ -4,11 +4,27 @@ import com.bearify.discord.api.interaction.CommandInteraction;
 import com.bearify.discord.spring.annotation.Command;
 import com.bearify.discord.spring.annotation.Interaction;
 
+import java.util.List;
+import java.util.Random;
+
 @Command
 public class PingCommand {
 
+    private static final List<String> RESPONSES = List.of(
+            "Pong! 🏓",
+            "Still hibernating... wait, no. 🐻",
+            "ROAR! 🐾",
+            "Grr! 😤🐻",
+            "The bear is awake. 👀",
+            "Did someone say honey? 🍯",
+            "Bearify is very much alive. 🎵🐻",
+            "Pong! Now leave me alone. 🐻💤"
+    );
+
+    private final Random random = new Random();
+
     @Interaction(value = "ping", description = "Check if the bot is alive")
     public void ping(CommandInteraction interaction) {
-        interaction.reply("Pong!").send();
+        interaction.reply(RESPONSES.get(random.nextInt(RESPONSES.size()))).ephemeral().send();
     }
 }
